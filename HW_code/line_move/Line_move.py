a8c92fd3d8cc28812cf22cbd38ad1da3715583e0
fbqweasd <fbqweasd2911@gmail.com>
2018-04-29 11:39:45 +0900

    Line_move(2)

--- HW_code/line_move/Line_move.py
+++ HW_code/line_move/Line_move.py
@@ -1,1 +1,15 @@
-import 
+import RPi.GPIO as GPIO
+from AlphaBot2 import AlphaBot2
+from TRSensors import TRSensor
+import time
+
+TR = TRSensor
+Ab = AlphaBot2
+
+while(1):
+    f = 0
+    input = TR.AnalogRead()
+    for i in range(0,4):
+        if(input[i] < 300):
+            f += 2^i
+    print (bin(f)) 
