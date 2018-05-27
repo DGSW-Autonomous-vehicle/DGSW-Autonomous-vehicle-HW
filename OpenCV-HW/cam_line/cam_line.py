d259b82052bbe5d84577b36e360dada22023482e
fbqweasd <fbqweasd2911@gmail.com>
2018-05-27 17:23:28 +0900

    cam_line_key

--- OpenCV-HW/cam_line/cam_line.py
+++ OpenCV-HW/cam_line/cam_line.py
@@ -8,34 +8,39 @@ from Camera_Line import Camera_Line
 Ab = AlphaBot2()
 
 CL = Camera_Line()
+CL.daemon = True
 CL.start()
 
-while(ture):
+try:
+    while(ture):
 
-    flag_CL = CL.flag
+        flag_CL = CL.flag
 
-    if(flag_CL == -1):
-        Ab.stop()
-        print("no input data")
+        if(flag_CL == -1):
+            Ab.stop()
+            print("no input data")
 
-    elif(flag_CL == 0):
-        Ab.motor_init()
-        Ab.forward()
-        print("forward")
+        elif(flag_CL == 0):
+            Ab.motor_init()
+            Ab.forward()
+            print("forward")
 
-    elif(flag_CL == 1):
-        print("right")
-        Ab.setMotor_UKC(-1, 1)
-        Ab.forward()
+        elif(flag_CL == 1):
+            print("right")
+            Ab.setMotor_UKC(-1, 1)
+            Ab.forward()
 
-    elif (flag_CL == 2):
-        print ("left")
-        Ab.setMotor_UKC(1, -1)
-        Ab.forward()
+        elif (flag_CL == 2):
+            print ("left")
+            Ab.setMotor_UKC(1, -1)
+            Ab.forward()
 
-    else:
-        Ab.motor_init()
-        Ab.stop()
-        print("Error")
+        else:
+            Ab.motor_init()
+            Ab.stop()
+            print("Error")
 
-    time.sleep(0.05)
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
