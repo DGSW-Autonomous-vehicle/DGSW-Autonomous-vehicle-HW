f77b8888c0dd1c986e208c5c772469721047bd74
fbqweasd <fbqweasd2911@gmail.com>
2018-07-14 11:17:02 +0900

    C++ GPIO test

---
+++ HW_code/C++/기본 파일/UKC_move.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+#define ain1 26
+#define ain2 23
+#define ena 22
+
+#define bin1 28
+#define bin2 29
+#define enb 22
+
+
+using namespace std;
+
+void init_wringPi();
+
+int main(){
+    init_wringPi();
+}
+
+void init_wringPi(){
+    if(wiringPiSetup() == -1){
+        cout << "WirginPi Setup Error" << endl;   
+        return 1;
+    }
+
+    pinMode(ain1,OUTPUT);
+    pinMode(ain2,OUTPUT);
+    pinMode(ena,OUTPUT);
+
+
+    pinMode(bin1,OUTPUT);
+    pinMode(bin2,OUTPUT);
+    pinMode(enb,OUTPUT);
+
+}
