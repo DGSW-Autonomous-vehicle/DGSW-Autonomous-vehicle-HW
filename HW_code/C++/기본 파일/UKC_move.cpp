19575d4f20b2b173ea030f4de10f9e9bbb464437
fbqweasd <fbqweasd2911@gmail.com>
2018-07-14 14:35:23 +0900

    C++ 초기 코드

--- HW_code/C++/기본 파일/UKC_move.cpp
+++ HW_code/C++/기본 파일/UKC_move.cpp
@@ -3,20 +3,80 @@
 #include <wiringPi.h>
 #include <softPwm.h>
 
+//왼쪽 바퀴 핀
 #define ain1 26
 #define ain2 23
-#define ena 22
 
+//오른쪽 바퀴 핀
 #define bin1 28
 #define bin2 29
-#define enb 22
 
-char ENA = 100;
-char ENB = 100;
+//왼쪽 바퀴 PWM핀
+#define ena 22
+
+//오른쪽 바퀴 PWM핀
+#define enb 22
 
 using namespace std;
 
+// 속도값 변수
+char ENA = 100;
+char ENB = 100;
+
+//////////////// 자치차량 함수 선언부 /////////////////////////
+
+// 초기화 함수
 void init_wringPi();
+
+// 움직임제어 함수 
+void forward();
+void stop();
+void back();
+void left();
+void right();
+
+//속도값 제어함수
+void setPWM(int a,int b)); 
+void setPWMA(int a); // 왼쪽 바퀴
+void setPWMB(int a); // 오른쪽 바퀴
+
+//////////////////////////////////////////////////////////////
+
+
+//// 우리의 개쩌는 main 함수님 ////
+int main(){
+
+    init_wringPi(); //초기화 코드
+
+    while(1) { 
+
+        forword();
+
+    }
+}
+
+/////////////////// 초기화 함수 /////////////////
+
+void init_wringPi(){
+    if(wiringPiSetup() == -1){
+        cout << "WirginPi Setup Error" << endl;   
+        return;
+    }
+
+    pinMode(ain1,OUTPUT);
+    pinMode(ain2,OUTPUT);
+    pinMode(ena,OUTPUT);
+
+    pinMode(bin1,OUTPUT);
+    pinMode(bin2,OUTPUT);
+    pinMode(enb,OUTPUT);
+
+    softPwmCreate(ena,0,255);
+    softPwmCreate(enb,0,255);
+}
+
+
+//////////////////// 움직임 제어함수 /////////////
 
 void forward(){
    softPwmWrite(ena,ENA);
@@ -38,39 +98,47 @@ void stop(){
    digitalWrite(bin2,LOW);
 }
 
-int main(){
-    init_wringPi();
-    while(1) {
-        forword();
-    }
+void back(){
+    softPwmWrite(ena,50);
+    softPwmWrite(enb,50);
+
+    digitalWrite(ain1,HIGH);
+    digitalWrite(ain2,LOW);
+    digitalWrite(bin1,HIGH);
+    digitalWrite(bin2,LOW);
 }
 
-void init_wringPi(){
-    if(wiringPiSetup() == -1){
-        cout << "WirginPi Setup Error" << endl;   
-        return;
-    }
+void right(){
+    softPwmWrite(ena,30);
+    softPwmWrite(enb,30);
 
-    pinMode(ain1,OUTPUT);
-    pinMode(ain2,OUTPUT);
-    pinMode(ena,OUTPUT);
-
-    pinMode(bin1,OUTPUT);
-    pinMode(bin2,OUTPUT);
-    pinMode(enb,OUTPUT);
-
-    softPwmCreate(ena,0,255);
-    softPwmCreate(enb,0,255);
+    digitalWrite(ain1,LOW);
+    digitalWrite(ain2,HIGH);
+    digitalWrite(bin1,HIHG);
+    digitalWrite(bin2,LOW);
 }
 
-void forward(){
-   softPwmWrite(ena,ENA);
-   softPwmWrite(enb,ENB);
+void left(){
+    softPwmWrite(ena,30);
+    softPwmWrite(enb,30);
 
-   digitalWrite(ain1,LOW);
-   digitalWrite(ain2,HIGH);
-   digitalWrite(bin1,LOW);
-   digitalWrite(bin2,HIGH);
+    digitalWrite(ain1,HIGH);
+    digitalWrite(ain2,LOW);
+    digitalWrite(bin1,LOW);
+    digitalWrite(bin2,HIGH);
 }
 
+/////////////// 속도값 제어 함수 ///////////////////
 
+void setPWM(int a, int b){
+
+}
+
+void setPWMA(int a){
+
+
+}
+
+void setPWMB(int a){
+
+}
